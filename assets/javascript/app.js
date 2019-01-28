e7a26ef21c2d10f628ce7833381a9ec7ab9541a8
Jason Schutz <jason@rogueathletic.com>
2019-01-27 18:21:34 -0800

    Update app.js
    final update fixing broken code form class yesterday

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,137 +1,175 @@
-
-var config = {
-    apiKey: "AIzaSyDuyThKpM3Rx2-4pGLlnVv4fSRCZaMOdb4",
-    authDomain: "train-co.firebaseapp.com",
-    databaseURL: "https://train-co.firebaseio.com",
-    projectId: "train-co",
-    storageBucket: "train-co.appspot.com",
-    messagingSenderId: "1093361277513"
+  // Initialize Firebase
+  var config = {
+      apiKey: "AIzaSyDuyThKpM3Rx2-4pGLlnVv4fSRCZaMOdb4",
+      authDomain: "train-co.firebaseapp.com",
+      databaseURL: "https://train-co.firebaseio.com",
+      projectId: "train-co",
+      storageBucket: "train-co.appspot.com",
+      messagingSenderId: "1093361277513"
   };
-   firebase.initializeApp(config);
-var trains = [];
-$(document).ready(function () {
- 
-	$(".content-box").hide();
-	$(".contorol").click(function() {
-		    $( this ).next( ".content-box" ).slideToggle().siblings( ".content-box" ).slideUp();
-		    if ($( "i" ).hasClass( "fa-remove" )) {
-			$( this ).find( "i" ).toggleClass( "fa-remove"  );         
-        }
-     });
+  firebase.initializeApp(config);
 
-});
-    var engineDestination   = "";
-    var arrivalFrequency    = "";
-    var nextArrival         = "";
-    var minutesAway         = "";
-    var trainTime           = "";
-    var elengineDestination = $( "#train-destination" );
-    var elTrainTime         = $( "#train-time" ).mask( "00:00" );
-    var elTimeFreq          = $(  "#time-freq" ).mask( "00" );
-    var elTrain             = $( "#train-name" );
+  $(document).ready(function () {
 
-    var database = firebase.database();
-        database.ref("/trains").on("child_added", function (snapshot) {
-        trains.push(snapshot.val());
-    render();
+      $(".content-box").hide();
+      $(".contorol").click(function () {
+          $(this).next(".content-box").slideToggle().siblings(".content-box").slideUp();
+          if ($("i").hasClass("fa-remove")) {
+              $(this).find("i").toggleClass("fa-remove");
+
+          }
+
+      });
+
+  });
 
 
-
-});
-
-function render() {
-    $('#table-data').empty();
-    for (let i = 0; i < trains.length; i++) {
-        const train = trains[i];
-        var trainDiff = 0;
-        var trainRemainder = 0;
-        var frequency = train.frequency;
-
-            trainDiff = moment().diff(moment.unix(train.time), "minutes");
-
-            trainRemainder = trainDiff % frequency;
-
-            train.minutesTillArrival = frequency - trainRemainder;
-
-            train.nextTrainTime = moment().add(train.minutesTillArrival, "m").format("hh:mm A");
-    }   
-    trains.sort(function(a, b) {
-        var trainATime = a.nextTrainTime.valueOf();
-        var trainBTime = b.nextTrainTime.valueOf();
-        if (trainATime < trainBTime) return -1;
-        if (trainATime > trainBTime) return 1;
-        return 0;
-    });
-    for (let i = 0; i < trains.length; i++) {
-        const train = trains[i];
-        $("#table-data").prepend(
-            "<tr class='theRow'><td>"  + train.name + "</td>" +
-            "<td class='dest'>"        + train.destination + "</td>" +
-            "<td class='freq'>"        + train.frequency + "</td>" +
-            "<td class='minTill'>"     + train.minutesTillArrival + "</td>" +
-            "<td class='nextTrain'>"   + train.nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true' id='exitX'></span></a>"    + "</td></tr>"
-            
-        );
-    }
-    $( " .theRow" ).click( function(event){
-        $( this ).hide().slow()
-        console.log()
-    });
-}
-
-    var storeInputs = function (event) {
-  
-             event.preventDefault();
-             trainName = elTrain.val().trim();
-             engineDestination = elengineDestination.val().trim();
-             trainTime = moment(elTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
-             arrivalFrequency = elTimeFreq.val().trim();
-
-        var trainObj = {
-            name: trainName,
-            destination: engineDestination,
-            time: trainTime,
-            frequency: arrivalFrequency,
-            nextArrival: nextArrival,
-            minutesAway: minutesAway,
-            date_added: firebase.database.ServerValue.TIMESTAMP
-        };
-             trains.push(trainObj);
-            database.ref( "/trains" ).push();
-
-        alert( "All Aboard!" );
-
-            elTrain.val( "" );
-            elengineDestination.val( "" );
-            elTrainTime.val(  "" );
-            elTimeFreq.val( "" );
-};
-
-$( "#btn-add" ).on( "click", function (event) {
-    
-    if ( elTrain.val().length === 0 || elengineDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0 ) {
-        alert( "Please Fill All Required Fields" );
-    }   else {
-        
-        storeInputs(event);
-    }
-
-});
-
-$('form').on("keyup", function (event) {
-    if (event.which === 13) {
-       
-    if (elTrain.val().length === 0 || elengineDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
-            alert("Please Fill All Required Fields");
-  } else {
-           
-            storeInputs(event);
-        }
-    }
-   
+  var engineDestination = "";
+  var arrivalFrequency = "";
+  var nextArrival = "";
+  var minutesAway = "";
+  var trainTime = "";
+  var elengineDestination = $("#train-destination");
+  var elTrainTime = $("#train-time").mask("00:00");
+  var elTimeFreq = $("#time-freq").mask("00");
+  var elTrain = $("#train-name");
 
 
-     $( '#dtBasicExample' ).DataTable();
-     $( '.dataTables_length' ).addClass( 'bs-select' );
-});
 
+
+
+
+
+
+  // ++++++++++++++++++++++++++
+
+
+
+
+  // ++++++++++++++++++++++++++
+
+
+
+  // Assign the reference to the database to a variable named 'database'
+  var database = firebase.database();
+
+  database.ref("/trains").on("child_added", function (snapshot) {
+
+      //  create local variables to store the data from firebase
+      var trainDiff = 0;
+      var trainRemainder = 0;
+      var minutesTillArrival = "";
+      var nextTrainTime = "";
+      var frequency = snapshot.val().frequency;
+
+      // compute the difference in time from 'now' and the first train using UNIX timestamp, store in var and convert to minutes
+      trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
+
+      // get the remainder of time by using 'moderator' with the frequency & time difference, store in var
+      trainRemainder = trainDiff % frequency;
+
+      // subtract the remainder from the frequency, store in var
+      minutesTillArrival = frequency - trainRemainder;
+
+      // add minutesTillArrival to now, to find next train & convert to standard time format
+      nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+
+      // append to our table of trains, inside tbody, with a new row of the train data
+      $("#table-data").append(
+          "<tr><td>" + snapshot.val().name + "</td>" +
+          "<td>" + snapshot.val().destination + "</td>" +
+          "<td>" + frequency + "</td>" +
+          "<td>" + minutesTillArrival + "</td>" +
+          "<td>" + nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+
+      );
+
+      $("span").hide();
+
+      // Hover view of delete button
+      $("tr").hover(
+          function () {
+              $(this).find("span").show();
+          },
+          function () {
+              $(this).find("span").hide();
+          });
+
+      // STARTED BONUS TO REMOVE ITEMS ** not finished **
+      $("#table-data").on("click", "tr span", function () {
+          console.log(this);
+          var trainRef = database.ref("/trains/");
+          console.log(trainRef);
+
+      });
+
+  });
+
+  // function to call the button event, and store the values in the input form
+  var storeInputs = function (event) {
+      // prevent from from reseting
+      event.preventDefault();
+
+      // get & store input values
+      trainName = elTrain.val().trim();
+      engineDestination = elengineDestination.val().trim();
+      trainTime = moment(elTrainTime.val().trim(), "HH:mm").subtract(1, "years").format("X");
+      arrivalFrequency = elTimeFreq.val().trim();
+
+      // add to firebase databse
+      database.ref("/trains").push({
+          name: trainName,
+          destination: engineDestination,
+          time: trainTime,
+          frequency: arrivalFrequency,
+          nextArrival: nextArrival,
+          minutesAway: minutesAway,
+          date_added: firebase.database.ServerValue.TIMESTAMP
+      });
+
+      //  alert that train was added
+      alert("All Aboard!");
+
+      //  empty form once submitted
+      elTrain.val("");
+      elengineDestination.val("");
+      elTrainTime.val("");
+      elTimeFreq.val("");
+  };
+
+  // Calls storeInputs function if submit button clicked
+  $("#btn-add").on("click", function (event) {
+      // form validation - if empty - alert
+      if (elTrain.val().length === 0 || elengineDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
+          alert("Please Fill All Required Fields");
+      } else {
+          // if form is filled out, run function
+          storeInputs(event);
+      }
+  });
+
+  // Calls storeInputs function if enter key is clicked
+  $('form').on("keypress", function (event) {
+      if (event.which === 13) {
+          // form validation - if empty - alert
+          if (elTrain.val().length === 0 || elengineDestination.val().length === 0 || elTrainTime.val().length === 0 || elTimeFreq === 0) {
+              alert("Please Fill All Required Fields");
+          } else {
+              // if form is filled out, run function
+              storeInputs(event);
+          }
+      }
+      // prepends a value space if number is < 10
+      if (minutes < 10) {
+          minutes = "0" + minutes
+
+
+      };
+      if (hour == 0 || hour > 12) {
+          hour = ((hour + 11) % 12 + 1); //allows to display ante meridiem time
+      }
+      $('#dtBasicExample').DataTable();
+      $('.dataTables_length').addClass('bs-select');
+
+
+  });
